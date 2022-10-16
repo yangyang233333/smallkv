c72dd8754423f17eab8cb907be2d927661297541
qianyy <hfutqyy@163.com>
2022-10-17 02:13:30 +0800

    修改文件架构

---
+++ smallkv/src/write.cpp
@@ -0,0 +1,5 @@
+//
+// Created by qianyy on 10/17/22.
+//
+
+#include "write.h"
---
+++ smallkv/src/write.h
@@ -0,0 +1,14 @@
+//
+// Created by qianyy on 10/17/22.
+//
+
+#ifndef SMALLKV_WRITE_H
+#define SMALLKV_WRITE_H
+
+
+class write {
+
+};
+
+
+#endif //SMALLKV_WRITE_H
